b6737c083472005b82fee1c54d6bdabeb4e818f4
unknown <elakavicius@gmail.com>
2019-05-01 20:29:42 +0300

    navbar added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
+  isIn:boolean = false;
 
-  constructor() { }
+    onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 
-  ngOnInit() {
+    toggleState() {
+      let bool = this.isIn;
+      this.isIn = bool === false ? true : false; 
   }
-
 }
